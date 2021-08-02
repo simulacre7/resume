31f2cade9fc6bc9495ca14a3628d22ee25a3be47
Kihwan Kim <kihwan_kim@tmax.co.kr>
2021-08-02 16:08:13 +0900

    티맥스소프트 내용 추가 및 학교 내용 일부 추가

--- src/components/TwoColumn/Education.jsx
+++ src/components/TwoColumn/Education.jsx
@@ -21,26 +21,34 @@ export default function Education() {
         <RowLeftCell
           companyName="울산과학기술원 (UNIST)"
           position="컴퓨터공학 석사"
-          time="2018.03-2020.02"
+          time="2018.03&ndash;2020.02"
           position2="기술경영학 학사 (컴퓨터공학 융합전공)"
-          time2="2013.03-2018.02"
+          time2="2013.03&ndash;2018.02"
         />
         <RowRightCell>
           <Project
             isFirst
-            title="연구1"
-            time="2021.01-현재"
-            description="설명1"
-            did={["1st item", "2nd item", "3rd item"]}
-            techStack="Typescript, React, Sass"
+            title="AI 중심 UI/UX 설계"
+            time="2019.01&ndash;2020.02"
+            description={
+              <>
+                "어떻게 UI를 구성해야 사용자의 취향이 담긴 데이터를 더 잘 수집할
+                수 있을까?"라는 질문에 답하는 연구를 수행하였습니다.
+              </>
+            }
+            did={[
+              "실험 환경으로 사용된 웹 기반 영화 추천 시스템 구현",
+              "사용자 로그의 가치를 AI 측면에서 평가하는 metric 제안",
+              "추천시스템의 Explore-Exploit 문제에서 투명성이 데이터의 품질에 미치는 영향을 측정",
+            ]}
+            techStack="Python, Flask, jQuery, Surprise"
           />
           <Project
-            isFirst
-            title="연구2"
-            time="2020.02-현재"
+            title="사용자 행동 모델링"
+            time="2019.02&ndash;현재"
             description="설명2"
-            did={["1st item", "2nd item", "3rd item"]}
-            techStack="Typescript, React, Sass"
+            did={["Association Rule", "Inverse Reinforcement Learning"]}
+            techStack="Python, TensorFlow, Keras, scikit-learn"
           />
         </RowRightCell>
       </Row>
--- src/components/TwoColumn/WorkExperience.jsx
+++ src/components/TwoColumn/WorkExperience.jsx
@@ -21,24 +21,41 @@ export default function WorkExperience() {
         <RowLeftCell
           companyName="티맥스소프트"
           position="Researcher"
-          time="2020.02-현재"
+          time="2020.02&ndash;현재"
         />
         <RowRightCell>
           <Project
             isFirst
-            title="사내 프론트엔드 모듈 개발"
-            time="2021.01-현재"
-            description="어쩌구 저쩌구 유지보수 개선 및 개발 연구하고 있습니다."
-            did={["1st item", "2nd item", "3rd item"]}
-            techStack="Typescript, React, Sass"
+            title="레거시 시스템을 리액트로 이전"
+            time="2021.01&ndash;현재"
+            description={
+              <>
+                jQuery 기반 사내 프론트엔드 라이브러리 &mdash; TOP
+                <i>(Tmax One Platform)</i>로 구현된 제품들을 리액트로 전환함
+              </>
+            }
+            did={[
+              <>
+                모듈화가 미흡한 기존 코드를 용도에 맞게 분할하고, <br />
+                인터페이스를 개선
+              </>,
+              "각종 컴포넌트 신규 개발",
+            ]}
+            techStack="Typescript, React, Sass, Material-UI"
           />
           <Project
-            isFirst
-            title="AutoML 프론트엔드 개발"
-            time="2020.02-현재"
-            description="어쩌구 저쩌구 유지보수 개선 및 개발 연구하고 있습니다."
-            did={["1st item", "2nd item", "3rd item"]}
-            techStack="Typescript, React, Sass"
+            title="AutoML 플랫폼"
+            time="2020.02&ndash;현재"
+            description="소모적이고 반복적인 기계학습 모델 개발 작업을 자동화해주는 플랫폼을 개발 했습니다."
+            did={[
+              "비전문가를 위한 Codeless 환경의 개발 스튜디오 구현",
+              <>
+                설명 가능한 AI &mdash; XAI
+                <i>(eXplainable Artificial Intelligence)</i> 기술 연구 및 개발
+              </>,
+              "컴퓨팅 리소스 관리 도구 개발",
+            ]}
+            techStack="Typescript, React, Sass, Material-UI, Python"
           />
         </RowRightCell>
       </Row>
