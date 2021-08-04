a13cf317bc270174805eb31087acdafcba2a8b2d
Kihwan Kim <kihwan_kim@tmax.co.kr>
2021-08-04 15:29:28 +0900

    Add contact link

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Typography, useMediaQuery } from "@material-ui/core";
 import ColorHead from "./ColorHead";
 import useScrollFadeIn from "../hooks/useScrollFadeIn";
+import "./AboutMe.scss";
 
 export default function AboutMe() {
   const matches = useMediaQuery("(max-width:960px)");
@@ -12,6 +13,7 @@ export default function AboutMe() {
   const animatedItem = {
     title: useScrollFadeIn("down"),
     greeting: useScrollFadeIn("down"),
+    contact: useScrollFadeIn("down"),
   };
   return (
     <section style={{ padding: "0 2rem" }}>
@@ -32,6 +34,18 @@ export default function AboutMe() {
         가치를 AI 측면에서 평가하고, 이를 통해 더 나은 인터페이스가 무엇인지를
         비교하는 연구를 수행했습니다.
       </Typography>
+      <div
+        ref={animatedItem.contact.ref}
+        style={animatedItem.contact.style}
+        id="contact"
+      >
+        <a id="email" href="mailto:juljin1875@gmail.com">
+          <Typography>juljin1875@gmail.com</Typography>
+        </a>
+        <a id="github" href="https://github.com/simulacre7/">
+          <Typography>GitHub</Typography>
+        </a>
+      </div>
     </section>
   );
 }
