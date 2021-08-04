bd0cffa4073914bfabba82dd6a5fc67ff6ef1d19
Kihwan Kim <kihwan_kim@tmax.co.kr>
2021-08-04 13:53:35 +0900

    add transition

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
+import useScrollFadeIn from "../hooks/useScrollFadeIn";
 
 export default function Badge({ children, style }) {
+  const animatedItem = {
+    badge: useScrollFadeIn("down"),
+  };
   return (
     <div
+      ref={animatedItem.badge.ref}
       style={{
+        ...animatedItem.badge.style,
         backgroundColor: "#e3f5ef",
         color: "teal",
         borderRadius: "0.2rem",
