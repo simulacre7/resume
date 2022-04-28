4ac48ff2e49ea1a2e84b55e585f9ca107f08d440
Kihwan Kim <juljin1875@gmail.com>
2022-04-29 03:46:13 +0900

    Animation 시 위치 조정 크기를 좀 더 작게 수정

--- src/hooks/useScrollFadeIn.jsx
+++ src/hooks/useScrollFadeIn.jsx
@@ -6,7 +6,7 @@ const useScrollFadeIn = (direction = "up", duration = 1, delay = 0) => {
   const handleDirection = (name) => {
     switch (name) {
       case "up":
-        return "translate3d(0, 3vh, 0)";
+        return "translate3d(0, 1vh, 0)";
       case "down":
         return "translate3d(0, -3vh, 0)";
       case "left":
