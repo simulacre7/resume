19883027c2d5a70370ffb04210671624a4473d9e
Kihwan Kim <juljin1875@gmail.com>
2022-04-28 18:19:58 +0900

    bezier 함수 변경: easeOutQuint

--- src/hooks/useScrollFadeIn.jsx
+++ src/hooks/useScrollFadeIn.jsx
@@ -24,7 +24,8 @@ const useScrollFadeIn = (direction = "up", duration = 1, delay = 0) => {
       if (entry.isIntersecting) {
         current.style.transitionProperty = "all";
         current.style.transitionDuration = `${duration}s`;
-        current.style.transitionTimingFunction = "cubic-bezier(0, 0, 0.2, 1)";
+        current.style.transitionTimingFunction =
+          "cubic-bezier(0.22, 1, 0.36, 1)";
         current.style.transitionDelay = `${delay}s`;
         current.style.opacity = 1;
         current.style.transform = "translate3d(0, 0, 0)";
